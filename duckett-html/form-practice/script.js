5f01b696c5f08aa2bb7a5a72d8417f8cd97699ad
Brian Short <brian.ralph.short@gmail.com>
2021-12-03 10:10:36 -0500

    Add rest of element variables for firs t section of form

--- duckett-html/form-practice/script.js
+++ duckett-html/form-practice/script.js
@@ -1,5 +1,19 @@
 const form = document.getElementById("characterForm");
 form.addEventListener("submit", event => {
     event.preventDefault();
-    console.log("Submitted");
+    const str = document.getElementById("str").value;
+    const dex = document.getElementById("dex").value;
+    const con = document.getElementById("con").value;
+    const wis = document.getElementById("wis").value;
+    const int = document.getElementById("int").value;
+    const chr = document.getElementById("chr").value;
+    const charObj = {
+        str,
+        dex,
+        con,
+        int,
+        wis,
+        chr
+    }
+    console.log(charObj);
 })
