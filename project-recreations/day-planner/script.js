f5bb1da74e984c6a577603228e133b088c378db2
Brian Short <brian.ralph.short@gmail.com>
2021-12-04 16:50:23 -0500

    Add comments

--- project-recreations/day-planner/script.js
+++ project-recreations/day-planner/script.js
@@ -34,8 +34,6 @@ const months = [
     "December"
 ]
 
-//console.log(months[month]);
-
 const today = document.getElementById("todays-date")
 today.innerText= `${days[day]}, ${months[month]} ${date.getDate()} ${date.getFullYear()}`;
 
@@ -69,11 +67,16 @@ clearButton.addEventListener("click", () => {
 const plannerContainer = document.getElementById("plannerContainer")
 
 for (var i = 1; i < 13; i++) {
+    //The realHour variable measures military time
     let realHour = i + 7;
+    //The displayHour variable measures clock hour display time, which
+    //will be used on the web page
     let displayHour = realHour;
     if (displayHour > 12) {
         displayHour -= 12;
     }
+    //Creates the div that displays the hour and adds
+    //text based on whether the hour is in the past or the future
     let div = document.createElement("div");
     div.innerText = `${displayHour} o'clock`;
     if (realHour < hour) {
@@ -84,22 +87,37 @@ for (var i = 1; i < 13; i++) {
         div.innerText += "|| Future";
     }
 
+    //Creates the input div where the user puts in there
+    //day planner plans
     let input = document.createElement("input");
     input.type = "text";
     input.id = `${displayHour}-value`;
+
+    //Creates the submit input element where the user
+    //clicks to save information to local storage 
+    //and for display
     let submit = document.createElement("input");
     submit.type = "submit";
     submit.id = `${displayHour}-submit`;
     submit.innerText = "Submit";
+    
+    //The event listener uses the logEntries function
+    //to store and display day planner input
     submit.addEventListener("click", (event) => {
         event.preventDefault();
         //console.log("Submitted");
         //console.log(displayHour);
         logEntries(displayHour.toString());
     });
+
+    //Append the display, input, and submit elements
     plannerContainer.append(div);
     plannerContainer.append(input);
     plannerContainer.append(submit);
+
+    //Creates a div to display content summoned from local storage;
+    //If there's nothing in local storage for that specific div to
+    //display, there is a default display text
     let plans = document.createElement("div");
     if (!localStorage.getItem(displayHour.toString())) {
         plans.innerText = "No plans";
@@ -109,8 +127,11 @@ for (var i = 1; i < 13; i++) {
     plannerContainer.append(plans);
 }
 
+//The logEntries function takes the specific input and stores
+//it in local storage and then reloads the location so new 
+//data can be displayed
 function logEntries(numString) {
-    console.log(`${numString}-value`)
+    //console.log(`${numString}-value`)
     let val = document.getElementById(`${numString}-value`);
     localStorage.setItem(numString, val.value);
     location.reload();
