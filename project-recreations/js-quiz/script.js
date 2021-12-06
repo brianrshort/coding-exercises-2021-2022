17b3f4799d59742e43f58f6a417032ef5d170ef0
Brian Short <brian.ralph.short@gmail.com>
2021-12-06 09:27:28 -0500

    Add document.getElementById variables

---
+++ project-recreations/js-quiz/script.js
@@ -0,0 +1,14 @@
+//timer score questions Q1 A1 A2 A3 A4 startGame nextQuestion newGame
+
+let timeField = document.getElementById("timer");
+let scoreField = document.getElementById("score");
+let questionField = docment.getElementById("questions");
+let questionArea = document.getElementById("Q1");
+let answerOne = document.getElementById("A1");
+let answerTwo = document.getElementById("A2");
+let answerThree = document.getElementById("A3");
+let answerFour = document.getElementById("A4");
+let startButton = document.getElementById("startGame");
+let nextButton = document.getElementById("nextQuestion");
+let newGameButton = document.getElementById("newGame");
+
