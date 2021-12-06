10867bc14487294def9dfce3c5643a9e03d34f8b
Brian Short <brian.ralph.short@gmail.com>
2021-12-06 11:03:38 -0500

    Add setTime function to run the game timer

--- project-recreations/js-quiz/script.js
+++ project-recreations/js-quiz/script.js
@@ -2,7 +2,7 @@
 
 let timeField = document.getElementById("timer");
 let scoreField = document.getElementById("score");
-let questionField = docment.getElementById("questions");
+let questionField = document.getElementById("questions");
 let questionArea = document.getElementById("Q1");
 let answerOne = document.getElementById("A1");
 let answerTwo = document.getElementById("A2");
@@ -16,3 +16,16 @@ let score = 0;
 let currentQuestionIndex;
 let secondsLeft = 100;
 
+function setTime() {
+    secondsLeft = 100;
+    let timerInterval = setInterval(() => {
+        timeField.innerText = `Seconds Left: ${secondsLeft}`;
+        secondsLeft--;
+        if (secondsLeft < 0) {
+            clearInterval(timerInterval);
+            timeField.innerText = "";
+        }
+    }, 1000)
+}
+
+setTime();
