8e543e80039e20e860a0b77ed7e733d5059f5d4e
Brian Short <brian.ralph.short@gmail.com>
2021-12-07 12:46:02 -0500

    Add event listener on button

--- project-recreations/password-generator/script.js
+++ project-recreations/password-generator/script.js
@@ -3,3 +3,10 @@ const caps = document.getElementById("PWType1");
 const lowers = document.getElementById("PWType2");
 const chars = document.getElementById("PWType3");
 const nums = document.getElementById("PWType4");
+const button = document.getElementById("genPW");
+
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(caps.checked);
+})
