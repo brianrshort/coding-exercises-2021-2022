002b49a25fedbd1575087a80d660fe19582c2b82
Brian Short <brian.ralph.short@gmail.com>
2021-12-07 12:49:55 -0500

    Add input for password length

--- project-recreations/password-generator/script.js
+++ project-recreations/password-generator/script.js
@@ -3,6 +3,7 @@ const caps = document.getElementById("PWType1");
 const lowers = document.getElementById("PWType2");
 const chars = document.getElementById("PWType3");
 const nums = document.getElementById("PWType4");
+const pwlength = document.getElementById("PWLength");
 const button = document.getElementById("genPW");
 
 
