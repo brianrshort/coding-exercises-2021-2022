55b42838e0219343894a8ae1e730a7fc514e7ccd
Brian Short <brian.ralph.short@gmail.com>
2021-12-07 12:43:06 -0500

    Add variables for html elements

---
+++ project-recreations/password-generator/script.js
@@ -0,0 +1,5 @@
+const passPlace = document.getElementById("PWplacement");
+const caps = document.getElementById("PWType1");
+const lowers = document.getElementById("PWType2");
+const chars = document.getElementById("PWType3");
+const nums = document.getElementById("PWType4");
