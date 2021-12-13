2ae593b412a92c5a0c42309376c0dc815f7d8960
Brian Short <brian.ralph.short@gmail.com>
2021-12-13 13:50:57 -0500

    Add rps server exercise file

---
+++ node-js/rps-server.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+// TODO: Require the http module
+
+
+
+// Function for handling POST responses
+function handlePostResponse(request, response){
+  request.setEncoding('utf8');
+  
+  // Receive chunks on 'data' event and concatenate to body variable
+  let body = '';
+  request.on('data', function (chunk) {
+    body += chunk;
+  });
+  
+  // When done receiving data, select a random choice for server
+  // Compare server choice with player's choice and send an appropriate message back
+  request.on('end', function () {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomChoice = choices[Math.floor(Math.random() * 3)];
+
+    const choice = body;
+
+    let message;
+
+    const tied = `Aww, we tied! I also chose ${randomChoice}.`;
+    const victory = `Dang it, you won! I chose ${randomChoice}.`;
+    const defeat = `Ha! You lost. I chose ${randomChoice}.`;
+
+    if (choice === randomChoice) {
+      message = tied;
+    } else if (
+        (choice === 'rock' && randomChoice === 'paper') ||
+        (choice === 'paper' && randomChoice === 'scissors') ||
+        (choice === 'scissors' && randomChoice === 'rock')
+    ) {
+      message = defeat;
+    } else {
+      message = victory;
+    }
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end(`You selected ${choice}. ${message}`);
+  });
+}
