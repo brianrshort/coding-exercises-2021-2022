2cbca93b16d2c783882aadc3a48a0b4d0248a75d
Brian Short <brian.ralph.short@gmail.com>
2021-12-14 15:00:02 -0500

    Add rooster.announceDawn test

--- tdd/rooster-test/index_text.js
+++ tdd/rooster-test/index_text.js
@@ -1,3 +1,11 @@
 const assert = require('assert');
 const Rooster = require('../index.js');
 
+describe('Rooster', () => {
+    describe(".announceDawn", () => {
+      it('returns a rooster call', () => {
+        const expected = 'cock-a-doodle-doo';
+        const announced = Rooster.announceDawn();
+        assert.strictEqual(expected, announced);
+      })
+    })
