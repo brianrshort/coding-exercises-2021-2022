a20710a13c051b05f95d1ff10d95ebadf6291a56
Brian Short <brian.ralph.short@gmail.com>
2021-12-15 14:21:37 -0500

    Add remaining code to calculate normal factorials

--- tdd/factorial/index.js
+++ tdd/factorial/index.js
@@ -2,7 +2,14 @@ const Calculate = {
     factorial: (num) => {
         if (num === 0) {
             return 1;
-          } 
+        } else {
+            let output = 1;
+            for (var i = 1; i <= num; i++) {
+              output *= i; 
+            }
+            return output;
+          }
+        } 
   }
   
   module.exports = Calculate;
