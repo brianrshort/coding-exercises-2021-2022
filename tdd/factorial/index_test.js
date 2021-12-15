312ddde3ed05421951a094ee38789d1058ceaf1e
Brian Short <brian.ralph.short@gmail.com>
2021-12-15 14:23:24 -0500

    Add unit test for 5!

--- tdd/factorial/index_test.js
+++ tdd/factorial/index_test.js
@@ -3,6 +3,11 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
     describe('.factorial', () => {
+        it('output of 5! is 120', () => {
+            const expectedResult = 120;
+            const result = Calculate.factorial(5);
+            assert.equal(result, expectedResult);
+          });
 
     });
   });
