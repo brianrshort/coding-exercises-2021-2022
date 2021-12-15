c710680f7f696a3355d5454181625e81225d759d
Brian Short <brian.ralph.short@gmail.com>
2021-12-15 14:23:40 -0500

    Add unit test for 8!

--- tdd/factorial/index_test.js
+++ tdd/factorial/index_test.js
@@ -8,6 +8,11 @@ describe('Calculate', () => {
             const result = Calculate.factorial(5);
             assert.equal(result, expectedResult);
           });
+          it('output of 8! is 40320', () => {
+            const expectedResult = 40320;
+            const result = Calculate.factorial(8);
+            assert.equal(result, expectedResult);
+          });
 
     });
   });
