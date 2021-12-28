0991c8fee6673ead26fce3cd8ef07e56a6ff3e78
Brian Short <brian.ralph.short@gmail.com>
2021-12-28 15:21:31 -0500

    Add functionality so when value of the font selector changes the font of the preview text changes

--- jquery/fonts/main.js
+++ jquery/fonts/main.js
@@ -2,4 +2,10 @@ $(document).ready(() => {
     $("#text").on("keyup", event => {
         $(".preview").html($(event.currentTarget).val());
       })
+      
+      $("#font").on("change", (event) => {
+        $(".preview").css({
+          fontFamily: $(event.currentTarget).val()
+        });
+      })
   })
