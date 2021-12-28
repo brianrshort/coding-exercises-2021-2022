cc8124b33b32517220bc44aebe1e0300ba2704f5
Brian Short <brian.ralph.short@gmail.com>
2021-12-28 13:24:40 -0500

    Add keyup function to count how many letters have been used in the twitter-esque functionality

--- jquery/feedster/main.js
+++ jquery/feedster/main.js
@@ -13,5 +13,16 @@ $(document).ready(() => {
         $(event.currentTarget).removeClass('btn-hover');
       })
 
+      $(".postText").focus();
+      $('.postText').on('keyup', (event) => {
+        let post = $(event.currentTarget).val();
+        let remaining = 140 - post.length;
+        $(".characters").html(remaining);
+        if (remaining <= 0) {
+          $('.wordcount').addClass('red');
+        } else if (remaining > 0) {
+          $(".wordcount").removeClass('red');
+        }
+      })
   }); 
   
