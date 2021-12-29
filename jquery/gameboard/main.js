43c47cf0476723065e0e0659f2c8963a46eef3c5
Brian Short <brian.ralph.short@gmail.com>
2021-12-29 10:00:00 -0500

    Add toggle for share menu

--- jquery/gameboard/main.js
+++ jquery/gameboard/main.js
@@ -2,6 +2,11 @@ var main = function() {
     $(".more-btn").on("click", (event) => {
         $(event.currentTarget).siblings().toggle();
       })
+      
+      $(".share").on("click", () => {
+        $(".share-menu").toggle();
+      })
+
     };
     
     $(document).ready(main);
