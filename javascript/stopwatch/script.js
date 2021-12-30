75449a785e0184a16ac63e0cb1f8422d82814a2a
Brian Short <brian.ralph.short@gmail.com>
2021-12-30 11:23:03 -0500

    Add time variables (minutes, seconds, milliseconds)

--- javascript/stopwatch/script.js
+++ javascript/stopwatch/script.js
@@ -6,3 +6,7 @@ let startButton = document.getElementById("start");
 let stopButton = document.getElementById("stop");
 let resetButton = document.getElementById("reset");
 
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+
