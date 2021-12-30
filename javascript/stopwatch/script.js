84601b74648271a6a52b49ca63840b3d82b884ab
Brian Short <brian.ralph.short@gmail.com>
2021-12-30 11:22:10 -0500

    Add html element variables to script.js

---
+++ javascript/stopwatch/script.js
@@ -0,0 +1,8 @@
+let minDisplay = document.getElementById("minutes");
+let secDisplay = document.getElementById("seconds");
+let milliDisplay = document.getElementById("milliseconds");
+
+let startButton = document.getElementById("start");
+let stopButton = document.getElementById("stop");
+let resetButton = document.getElementById("reset");
+
