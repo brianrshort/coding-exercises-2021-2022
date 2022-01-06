cb8cfae7a8b81b59c40c53903b271ce00bb18b4d
Brian Short <brian.ralph.short@gmail.com>
2022-01-06 13:39:17 -0500

    Fix typo in schema.sql

--- project-recreations/burger-app/db/schema.sql
+++ project-recreations/burger-app/db/schema.sql
@@ -7,7 +7,7 @@ USE burger_city_db;
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger varchar(255) NOT NULL,
-    devoured BOOLEAN default false;
+    devoured BOOLEAN default false,
     createdAt TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
