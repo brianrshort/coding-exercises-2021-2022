713eefacf876700b34d2bf5a48f5c73ee0b10faa
Brian Short <brian.ralph.short@gmail.com>
2022-01-06 08:53:04 -0500

    Add db folder for burger app; add schema.sql to set db name and table schema

---
+++ project-recreations/burger-app/db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burger_city_db;
+
+CREATE DATABASE burger_city_db;
+
+USE burger_city_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger varchar(255) NOT NULL,
+    devoured BOOLEAN default false;
+    createdAt TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
