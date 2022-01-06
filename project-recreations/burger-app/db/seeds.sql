22428d4312ab490210af2674156d199f44738486
Brian Short <brian.ralph.short@gmail.com>
2022-01-06 08:56:42 -0500

    Add seeds.sql file to add starting elements to db

---
+++ project-recreations/burger-app/db/seeds.sql
@@ -0,0 +1,10 @@
+USE burger_city_db;
+
+INSERT INTO burgers (burger) VALUES ('Good Morning Burger');
+INSERT INTO burgers (burger) VALUES ('Black Bean Vegan Guac Burger');
+INSERT INTO burgers (burger) VALUES ("Quad with Grilled Onions on a Kaiser");
+INSERT INTO burgers (burger) VALUES ("Chicken Pickle Burger");
+INSERT INTO burgers (burger) VALUES ("Purple Bean Burger");
+INSERT INTO burgers (burger) VALUES ('Grey Bean Burger');
+INSERT INTO burgers (burger) VALUES ("Big Kahuna Burger");
+INSERT INTO burgers (burger) VALUES ("Oyster Stuffing Burger");
