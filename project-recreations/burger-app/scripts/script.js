8db049cc4e9ed29793a1dce0c940b9f3e68b64df
Brian Short <brian.ralph.short@gmail.com>
2022-01-07 08:57:17 -0500

    Add initial fetch function to get display data

--- project-recreations/burger-app/scripts/script.js
+++ project-recreations/burger-app/scripts/script.js
@@ -2,3 +2,18 @@ let arr;
 let devouredArr;
 let uneatenArr;
 
+fetch('/').then(res => res.json()).then(data => {
+    //console.log(data);
+    arr = data;
+    console.log(arr);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].devoured = false) {
+            uneatenArr.push(arr[i]);
+        } else {
+            devouredArr.push(arr[i]);
+        }
+    };
+    console.log(uneatenArr);
+    console.log(devouredArr);
+});
+
