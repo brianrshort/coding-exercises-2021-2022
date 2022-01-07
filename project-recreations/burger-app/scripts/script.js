101e4f66ceae1afa0eb9bc114ee92595f3e39907
Brian Short <brian.ralph.short@gmail.com>
2022-01-07 08:57:54 -0500

    Add displayBurger function to display burger data from DB

--- project-recreations/burger-app/scripts/script.js
+++ project-recreations/burger-app/scripts/script.js
@@ -17,3 +17,30 @@ fetch('/').then(res => res.json()).then(data => {
     console.log(devouredArr);
 });
 
+function displayBurger(burgerObj) {
+    let div = document.createElement("div");
+    let header = document.createElement("h1");
+    header.innerText = burgerObj.burger;
+    div.append(header);
+    let devourButton = document.createElement("input");
+    devourButton.type = "submit";
+    devourButton.id = burgerObj.id;
+    devourButton.name = burgerObj.devoured;
+    devourButton.addEventListener('click', e => {
+        e.preventDefault();
+        devourBurger(e.target);
+        location.reload();
+    });
+    div.append(devourButton);
+    let deleteButton = document.createElement("input");
+    deleteButton.type = "submit";
+    deleteButton.id = burgerObj.id;
+    deleteButton.addEventListener('click', e => {
+        e.preventDefault();
+        deleteBurger(e.target.id);
+        location.reload();
+    });
+    div.append(deleteButton);
+    return div; 
+}
+
