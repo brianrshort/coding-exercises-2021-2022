1a1c67c9bdb824ee2d98457fe9858a336c89bd2f
Brian Short <brian.ralph.short@gmail.com>
2022-01-07 11:00:38 -0500

    Add res.sendFile for index.html

--- project-recreations/burger-app/controller/burger_controller.js
+++ project-recreations/burger-app/controller/burger_controller.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const connection = require('../config/connection');
+const path = require("path");
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+})
 
 
 router.get("/get/", (req, res) => {
